f6ed8997f00e5955025bfbebec0c5b03a183af52
Jp Gigo <jpg-leboss@mail.fr>
2020-12-17 18:57:14 +0100

    initial commit

---
+++ compare-fonction.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Nettoyage de l'extraction
+function menage() {
+awk '{ print $2 }' $SID > /tmp/$SID
+awk '{ print $2 }' $BUSTER > /tmp/$BUSTER
+}
+
+# On met en ordre
+function tri() {
+sort /tmp/$SID > /tmp/$SID"-1"
+sort /tmp/$BUSTER > /tmp/$BUSTER"-1"
+}
+
+# On cherche ce qui n'est ni dans A ni dans B et on enleve les tabulations
+function comm-3() {
+comm -3 /tmp/$SID"-1" /tmp/$BUSTER"-1" | sed -r "s/\t//ig" > paquets.txt
+}
+
+#On cherche ce qui est dans B et pas dans A et on enleve les tabulations
+function comm-2() {
+comm -2 /tmp/$SID"-1" /tmp/$BUSTER"-1" | sed -r "s/\t//ig" > paquets.txt
+}
+
+#On cherche ce qui est dans A et pas dans B et on enleve les tabulations
+function comm-1() {
+comm -1 /tmp/$SID"-1" /tmp/$BUSTER"-1" | sed -r "s/\t//ig" > paquets.txt
+}
